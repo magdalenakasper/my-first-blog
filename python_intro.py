fcd58464b4480771dc2c26714252b77d5bd013b0
Magdalena Kasper <magdalenakasper1992@gmail.com>
2019-09-28 14:32:25 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,62 @@
+print("Hello, Django girls")
+if 3>2:
+    print("To działa")
+if 5>2:
+    print("5 jest jednak większe od 2")
+else:
+    print("5 nie jest większe od 2")
+name="Sonja"
+if name == "Ola":
+    print("Hej Ola!")
+elif name == "Sonja":
+    print("Hej Sonja")
+else:
+    print("Hej anonimie")
+volume=57
+if volume<20:
+    print("It's kinda quiet.")
+elif 20 <=volume < 40:
+    print("It's nice for background music")
+elif 40<=volume <60:
+    print("Perfect, I can hear all the details")
+elif 60<=volume<80:
+    print("Nice for parties")
+elif 80 <=volume <100:
+    print("A bit loud")
+else:
+    print("My ears are hurting:(")
+#Change the volume if it's too loud or too quiet
+if volume <20 or volume > 80:
+    volume=50
+    print("That's better!")
+def hi():
+    print("Hej")
+    print("Jak się masz?")
+hi()
+print(hi)
+
+def hi(imie):
+    if imie =="Ola":
+        print("Hej Ola")
+    elif imie == "Sonja":
+        print("Hej Sonja")
+    else:
+        print("Hej nieznajoma")
+hi("Ola")
+hi("Sonja")
+hi("Magdalena")
+
+def hi(imie):
+    print("Hej" + imie + "!")
+hi("Rachel")
+dziewczyny=["Rachel","Monica","Phoebe","Ola","Ty"]
+
+def hi(imie):
+    print("Witaj"+ imie + "!")
+dziewczyny = ["Rachel","Monica", "Phoebe","Ola","Ty"]
+for i in dziewczyny:
+    hi(i)
+    print("Kolejna dziewczyna")
+
+for i in range(1,6):
+    print(i)    
